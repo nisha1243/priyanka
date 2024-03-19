d1d766c3618c0167642ddb5d9fc3c71ca3173014
nisha1243 <163998934+nisha1243@users.noreply.github.com>
2024-03-19 01:36:12 -0800

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,12 @@
+class demo1
+{
+   public static void main(String arg[])
+      {
+         int a,b,c;
+         a=30;
+         b=50;
+         c=a+b;
+         	
+   System.out.println(c);
+      }
+}
